4656c38466b184ca91860ad9e70163f540ab5249
JamiuKolawole04 <Jakko4real@gmail.com>
2022-04-28 16:52:55 +0100

    adding the server link

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/" // THE API (backend) URL
+    baseURL: "https://amazon-server-practice.herokuapp.com/" // THE API (backend) URL
 });
 
 export default instance;
